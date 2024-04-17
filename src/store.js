ae6edc3f2dd290baae1fb0e6d47bdd7ea6997c2c
danielbosa <danielbosa95@gmail.com>
2024-04-17 12:00:46 +0200

    Start: basic structure of store and api

---
+++ src/store.js
@@ -0,0 +1,20 @@
+import {reactive} from 'vue';
+
+export const store = reactive({
+    apiUrl: 'https://api.themoviedb.org/3/',
+    endPoint:
+    {
+        movie: 'search/movie',
+        tv: 'search/tv'
+    },
+    options:{
+        params:{
+            /*Ogni volta che faccio una chiamata api, passo api key */
+            api_key: '2b9c68b42b884cbe5e46d84cf477a0f5',
+            query: 'a',
+
+        }
+    },
+    /*Da concatenare con quello che mi arriva dalle API */
+    imageUrl: 'https://image.tmdb.org/t/p/original',
+})
